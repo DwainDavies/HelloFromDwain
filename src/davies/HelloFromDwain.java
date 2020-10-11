c9e538d2ca7ccbd2447bd7b924c2da24a9d74b8b
Maksim Vlasov <54456828+Mask22446@users.noreply.github.com>
2020-10-11 21:48:24 +0300

    Update HelloFromDwain.java

--- src/davies/HelloFromDwain.java
+++ src/davies/HelloFromDwain.java
@@ -4,5 +4,6 @@ public class HelloFromDwain {
 
     public static void main(String[] args) {
         System.out.println("Hello my name is Dwain");
+        System.out.println("Hello Dwain, my name is Maksim. Nice to meet you!");
     }
 }
